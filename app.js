a94c85ba07f76f979d64bdec800fd562b10da6c3
Bred09 <bred.king@bk.ru>
2020-01-12 16:11:44 +0500

    Working with REST Api1

--- app.js
+++ app.js
@@ -105,6 +105,46 @@ io.on('connection', function (socket) {
 });
 
 
+var sendNotification = function(data) {
+  var headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Basic ZDc3NzkzMDQtYjAxNi00YTg2LThhYTItMDczMzU2ZmJhY2Iz"
+  };
+  
+  var options = {
+    host: "onesignal.com",
+    port: 443,
+    path: "/api/v1/notifications",
+    method: "POST",
+    headers: headers
+  };
+  
+  var https = require('https');
+  var req = https.request(options, function(res) {  
+    res.on('data', function(data) {
+      console.log("Response:");
+      console.log(JSON.parse(data));
+    });
+  });
+  
+  req.on('error', function(e) {
+    console.log("ERROR:");
+    console.log(e);
+  });
+  
+  req.write(JSON.stringify(data));
+  req.end();
+};
+
+var message = { 
+  app_id: "4f74eaf6-6ee9-43a6-a7b1-fccf3f809129",
+  contents: {"en": "Hello! Bred"},
+  included_segments: ["All"]
+};
+
+sendNotification(message);
+
+
 // Start server
 http.listen(PORT, function() {
 	console.log(`Server Run... [${PORT}]`)
